41e5d4b99a01b459eda03587ce9172036ba8ab82
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2021-03-04 16:58:04 +0530

    Create a Program to add two no.s in C

---
+++ Basic programs/Adding 2 no.s
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main(){
+    int n1,n2,s;
+    printf("Enter the no.s whose sum you need to find");
+    scanf("%d %d",&n1 ,&n2);
+    s=n1+n2;
+    printf("Sum of %d and %d is %d ", n1,n2,s);
+}
