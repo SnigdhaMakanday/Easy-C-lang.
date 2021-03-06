fa05fed14e6970c3e5e521205aa3a138dc970406
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2021-03-06 23:29:27 +0530

    Write a Program to check whether a number is divisible by 97 or not

---
+++ Basic programs/divisibility.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main(){
+    int n ;
+    printf("Enter a number:");
+    scanf("%d",&n);
+    if(n%97==0){
+        printf("Divisible by 97");
+        
+    }
+    else{
+        printf("Not divisible by 97");
+    }
+}
