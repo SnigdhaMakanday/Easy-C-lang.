4811212b3dff6640314ce5f201bbb137fde965c1
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2021-03-19 16:15:59 +0530

    Write a program to print n natural numbers in reverse order

---
+++ Basic programs/Reverse.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main(){
+    int n,i;
+    printf("Enter the number till where you want to print");
+    scanf("%d",&n);
+    for(i=n;i>0;i--){
+        printf("%d\n",i);
+        
+    }
+}
