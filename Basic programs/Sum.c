b6ad48a5771c9b50c20890e91b551477fff58fd8
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2021-03-19 16:57:13 +0530

    Did the same question with the three loops: For, While, Do - while

--- Basic programs/Sum.c
+++ Basic programs/Sum.c
@@ -1,3 +1,4 @@
+---------------------------------FOR LOOP-----------------------------------------
 #include<stdio.h>
 void main(){
     int n ,i,s=0;
@@ -8,3 +9,33 @@ void main(){
     }
     printf("Sum = %d",s);
 }
+
+
+--------------------------------WHILE LOOP------------------------------------------
+#include<stdio.h>
+void main(){
+    int i = 1,n, s=0;
+    printf("Enter the no. till where you need Sum");
+    scanf("%d",&n);
+    while(i<=n){
+        s=s+i;
+        i++;
+    }
+    printf("Sum is = %d",s);
+}
+
+
+-------------------------Do - WHILE LOOP--------------------------------------------
+ #include<stdio.h>
+void main(){
+    int i = 1,n, s=0;
+    printf("Enter the no. till where you need Sum");
+    scanf("%d",&n);
+    do{
+        
+        s=s+i;
+        i++;
+    }while(i<=n);
+    printf("Sum is = %d",s);
+}
+
