7e552bcde99d9ef2417e981f3770a630377867fe
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2021-03-20 23:43:33 +0530

    Basic Function program

---
+++ Function/Basic_Func.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void GM();
+void GA();
+void GN();
+void main(){
+  GM();
+  GA();
+  GN();
+}
+void GM(){
+  printf("Good Morning");
+}
+void GA(){
+  printf("Good Afternoon");
+ 
+}
+void Gn(){
+  printf("Good Night");
+}
