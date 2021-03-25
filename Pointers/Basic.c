ff6975b4678cd4b171a6e587af56611c45a6b694
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2021-03-25 17:06:42 +0530

    A program to demonstrate pointers

---
+++ Pointers/Basic.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main(){
+    int i=8;
+    int *j;
+    j=&i;
+    printf("Add i = %u\n",&i);
+    printf("Add i = %u\n",j);
+    printf("Add j = %u\n",&j);
+    printf("Value i = %d\n",i);
+    printf("Value i = %d\n",*(&i));
+    printf("Value i = %d\n",*j);
+}
